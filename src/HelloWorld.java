657633ecf1cf98fef7147aef8d93615a279091bb
Troydon <50076962+TroydonAnabolic@users.noreply.github.com>
2025-09-21 20:40:40 +1200

    Initial commit: add HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+// Course: BIT504, Name: Troydon Luicien, StudentID: 5139394, Assessment BIT504_A2 Task9
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
